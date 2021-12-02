236fe1f12bf7bd52c34c6a3c652835c2f15f79db
Adam Hepton <adam.hepton@skybettingandgaming.com>
2021-12-02 10:56:36 +0000

    Formatting, mainly to make it easier to comment out by indent level to inspect program better

--- 02/directions.js
+++ 02/directions.js
@@ -4,30 +4,35 @@ fs.readFile('./directions.txt', function(err, data) {
     const directions = data.toString().split("\n").map(dir => dir.split(" "));
 
     const calculateDive = (method, directions) => {
-        return Object.values(directions
-                .reduce( (total, dirs) => {
-                    const dir = dirs[0], val = Number(dirs[1]);
+        return (
+            Object.values(
+                directions
+                    .reduce( (total, dirs) => {
+                        const dir = dirs[0], val = Number(dirs[1]);
 
-                    switch(dir) {
-                        case 'forward':
-                            total.forward += val;
-                            if(method === 2) {
-                                total.depth += (val * total.aim);
-                            }
-                            break;
-                            
-                        case 'down':
-                        case 'up':
-                            const targetAttribute = (method === 1 ? 'depth' : 'aim');
-                            total[targetAttribute] += (dir === 'down' ? val : -val);
-                            break;
-                    }
-                    
-                    return total;
-                }, { forward: 0, depth: 0, aim: 0 })
+                        switch(dir) {
+                            case 'forward':
+                                total.forward += val;
+                                if(method === 2) {
+                                    total.depth += (val * total.aim);
+                                }
+
+                                break;
+                                
+                            case 'down':
+                            case 'up':
+                                const targetAttribute = (method === 1 ? 'depth' : 'aim');
+                                total[targetAttribute] += (dir === 'down' ? val : -val);
+                                
+                                break;
+                        }
+                        
+                        return total;
+                    }, { forward: 0, depth: 0, aim: 0 })
+            )
+            .slice(0, 2)
+            .reduce( (a, b) => a * b)
         )
-        .slice(0, 2)
-        .reduce( (a, b) => a * b)
     }
 
     console.log(calculateDive(1, directions));
